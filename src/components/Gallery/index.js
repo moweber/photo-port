14e5a997476b8c01530fca99db0c7fa1faad572b
DESKTOP-NK6EJSL\moweber <moworldenterprises@gmail.com>
2022-10-09 21:37:27 -0500

    Finished lesson 20.3.4

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
+import photo from "../../assets/small/commercial/0.jpg";
+
+function Gallery() {
+  const currentGallery = {
+    name: "commercial",
+    description:
+      "Photos of grocery stores, food trucks, and other commercial projects",
+  };
+  return (
+    <section>
+      <h1>{capitalizeFirstLetter(currentGallery.name)}</h1>
+      <p>{currentGallery.description}</p>
+      <div>
+        <img
+          className="img-thumbnail mx-1"
+          src={photo}
+          alt="Commercial Example"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Gallery;
